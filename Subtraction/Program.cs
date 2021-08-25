01adbf2d14a720e112d932c4f06f265fa816271a
Hackmoses <89389925+Hackmoses@users.noreply.github.com>
2021-08-25 18:11:18 +0100

    Add files via upload

---
+++ Subtraction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 0;
+            int numberTwo = 0;
+
+            int result = 0;
+
+            Console.Write("Enter your firstnumber: ");
+            numberOne = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your secondnumber: ");
+            numberTwo = int.Parse(Console.ReadLine());
+
+            
+
+            result = numberOne - numberTwo ;
+
+
+            Console.WriteLine("The answer is : " + result);
+        
+        }
+    }
+}
