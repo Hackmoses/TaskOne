5920c3b71a0a499b0614e0090c96e47705c3cebb
Hackmoses <89389925+Hackmoses@users.noreply.github.com>
2021-08-25 18:05:00 +0100

    Add files via upload

---
+++ Multiplication/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 0;
+            int numberTwo = 0;
+            int numberThree = 0;
+            int result = 0;
+
+            Console.Write("Enter your firstnumber: ");
+            numberOne = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your secondnumber: ");
+            numberTwo = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your thirdnumber: ");
+            numberThree = int.Parse(Console.ReadLine());
+
+            result = numberOne * numberTwo *numberThree;
+
+
+            Console.WriteLine("The answer is : " + result);
+
+
+        }
+    }
+}
