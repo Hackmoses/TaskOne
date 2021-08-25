de021d6957f0ecff9d5745d7a0c2771848578a15
Hackmoses <89389925+Hackmoses@users.noreply.github.com>
2021-08-25 17:56:58 +0100

    Add files via upload

---
+++ Division/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 0;
+            int numberTwo = 0;
+
+            int result = 0;
+
+            Console.Write("Enter your firstnumber: ");
+            numberOne = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your secondnumber: ");
+            numberTwo = int.Parse(Console.ReadLine());
+
+            
+
+            result = numberOne / numberTwo ;
+
+
+            Console.WriteLine("The answer is : " + result);
+        }
+    }
+}
